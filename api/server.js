55019614c460289d05e8b5a51a82502b57192878
Pawel Zguda <zgudapawel@gmail.com>
2022-09-14 04:44:20 +0200

    creating all routes and utility to upload images localy

--- api/server.js
+++ api/server.js
@@ -5,11 +5,13 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 // app.use('/', (req, res) => {
 //   console.log('this is main url!');
 // });
-
+dotenv.config();
 app.use(express.json()); //  app to be able send json file in body, for example via postman. This is mainly for POST and PUT request. For GET, and DELETE dont need.
 
 app.use('/images', express.static(path.join(__dirname, '/images')));
@@ -26,7 +28,8 @@ const storage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    //cb(null, "myname.jpg")
+    // cb(null, "myname.jpg")
+    // cb(null, 'black-2.jpg');
     cb(null, req.body.name);
   },
 });
@@ -36,9 +39,9 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
   res.status(200).json('File has been uploaded');
 });
 
-app.use('./api/auth', authRoute);
-// app.use('./api/users', userRoute);
-// app.use('./api/posts', postRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 app.listen('5000', () => {
   console.log('Backend is running!');
