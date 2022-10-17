592f22ad800e2bda44e6808cdfae748f8c052303
Pawel Zguda <zgudapawel@gmail.com>
2022-10-17 22:53:20 +0200

    add spinner

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,10 @@ import css from './Home.module.css';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 
+import { SlidingPebbles } from 'react-spinner-animated';
+
+import 'react-spinner-animated/dist/index.css';
+
 // const posts = [
 //   {
 //     image: `https://placeimg.com/640/48${Math.floor(
@@ -24,14 +28,17 @@ import { useLocation } from 'react-router-dom';
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
-
+  const [isLoading, setIsLoading] = useState(true);
 
   const sortedPosts = posts.sort((a, b) => b.postDate - a.postDate);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get(process.env.REACT_APP_BACKEND_URL + `/posts${search}`);
+      const res = await axios.get(
+        process.env.REACT_APP_BACKEND_URL + `/posts${search}`
+      );
       setPosts(res.data);
+      res.data && setIsLoading(false);
     };
     fetchPosts();
   }, [search]);
@@ -40,7 +47,20 @@ const Home = () => {
     <>
       <Header />
       <div className={css.home}>
-        <Posts posts={sortedPosts} />
+        {isLoading ? (
+          <div className={css.spinner}>
+            <SlidingPebbles
+              text={'Loading...'}
+              bgColor={'transparent'}
+              center={false}
+              width={'150px'}
+              height={'150px'}
+            />
+          </div>
+        ) : (
+          <Posts posts={sortedPosts} />
+        )}
+
         <Sidebar extraStylesHome />
       </div>
     </>
